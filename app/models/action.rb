7db47ed945064c03f826e8dee662eadd0b71c94c
Omar Sahyoun <osahyoun@gmail.com>
2017-07-12 15:10:58 +0100

    Experiment: pass new actions to stream to be processed by lambda

--- app/models/action.rb
+++ app/models/action.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: actions
@@ -18,12 +19,46 @@
 #
 
 class Action < ApplicationRecord
+  STREAM = Aws::DynamoDB::Client.new(
+    region: Settings.aws_region
+  )
+
   belongs_to :page, counter_cache: :action_count
   belongs_to :member
+  after_create :push_to_stream
 
-  enum publish_status: [:default, :published, :hidden]
+  enum publish_status: %i[default published hidden]
 
-  has_paper_trail on: [:update, :destroy]
+  has_paper_trail on: %i[update destroy]
   scope :donation, -> { where(donation: true) }
   scope :not_donation, -> { where.not(donation: true) }
+
+  private
+
+  def clean_empty_strings(item)
+    enforce_types = {
+      datetime: :to_s,
+      integer: :to_s
+    }
+
+    item = item.each do |k, v|
+      type = Action.columns_hash[k]&.type
+      enforcer = enforce_types[type]
+
+      item[k] = v.send(enforcer) if enforcer
+      item[k] = false if type == :boolean && !item[k]
+      item[k] = nil if v.blank?
+      item[k] = clean_empty_strings(item[k]) if item[k].is_a? Hash
+    end
+
+    item
+  end
+
+  def push_to_stream
+    return unless Settings.stream_name
+    item = clean_empty_strings(item)
+
+    STREAM.put_item(item: item,
+                    table_name: Settings.action_stream_name)
+  end
 end
